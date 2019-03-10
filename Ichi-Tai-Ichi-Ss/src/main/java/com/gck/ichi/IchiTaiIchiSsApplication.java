d13cae99682d1de77b06a8a653cd583ee560f0e8
Ganesh Chaitanya Kale <ganesh2583@gmail.com>
2019-03-10 12:20:35 +0530

    Adding initial implementaion of the Server side. Created a new Spring boot project with H2 Db configured in the applicaiton.properties and Spring security enabled.

---
+++ Ichi-Tai-Ichi-Ss/src/main/java/com/gck/ichi/IchiTaiIchiSsApplication.java
@@ -0,0 +1,13 @@
+package com.gck.ichi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IchiTaiIchiSsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IchiTaiIchiSsApplication.class, args);
+	}
+
+}
---
+++ Ichi-Tai-Ichi-Ss/src/test/java/com/gck/ichi/IchiTaiIchiSsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gck.ichi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IchiTaiIchiSsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
